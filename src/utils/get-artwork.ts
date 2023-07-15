caa13f7d550af922dc498585192efeac8697aedf
bangimron <imron.rosadi@jatis.com>
2023-07-15 13:28:21 +0700

    init utils and func to get artwork

---
+++ src/utils/get-artwork.ts
@@ -0,0 +1,7 @@
+import env from "$/configs/env";
+
+const getArtwork = (id:number|string) => {
+  return `${env.imageUrl}${id}${env.imageFormat}`;
+}
+
+export default getArtwork
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export {default as getArtwork} from './get-artwork'
