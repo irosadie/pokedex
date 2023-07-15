bd8a4238df7a0ffa0c4a6c78ecf17aaa57322a7e
bangimron <imron.rosadi@jatis.com>
2023-07-16 01:49:44 +0700

    Feat: add card components

---
+++ src/components/card/card.variant.ts
@@ -0,0 +1,41 @@
+/* eslint-disable check-file/filename-naming-convention */
+import { cva } from 'class-variance-authority'
+import styles from './card.module.scss'
+
+export const card = cva(styles.card, {
+    variants: {
+        intent: {
+            white: styles.i_white,
+            black: styles.i_black,
+            primary: styles.i_primary,
+            light: styles.i_light,
+            normal: styles.i_normal,
+            fire: styles.i_fire,
+            water: styles.i_water,
+            electric: styles.i_electric,
+            grass: styles.i_grass,
+            ice: styles.i_ice,
+            fighting: styles.i_fighting,
+            poison: styles.i_poison,
+            ground: styles.i_ground,
+            flying: styles.i_flying,
+            psychic: styles.i_psychic,
+            bug: styles.i_bug,
+            rock: styles.i_rock,
+            ghost: styles.i_ghost,
+            dragon: styles.i_dragon,
+            dark: styles.i_dark,
+            steel: styles.i_steel,
+            fairy: styles.i_fairy,
+            unknow: styles.i_unknow,
+        },
+        dimension: {
+            normal: styles.d_normal,
+        }
+    },
+    compoundVariants: [],
+    defaultVariants: {
+        intent: 'primary',
+        dimension: 'normal',
+    },
+})
