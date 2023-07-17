61a01d74ff63aabab7fd7291efc7a78e1bf76b44
bangimron <imron.rosadi@jatis.com>
2023-07-17 07:31:25 +0700

    feat: home page

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,6 +3,8 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RecoilRoot } from 'recoil'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function AppTemplate({
   children,
@@ -16,6 +18,14 @@ export default function AppTemplate({
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         {children}
+        <ToastContainer
+          position="top-right"
+          autoClose={7000}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </QueryClientProvider>
     </RecoilRoot>
   )
